d433ed9aae3f548dad3e1b56186973cadb6be5f4
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-09-19 12:06:48 +0530

    changes

---
+++ src/PortforlioContainer/Home/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+          <div className='profile-parent'>
+             <div className='profile-details'>
+                <div className='colz'>
+                    <a href='#'>
+                        <i className="fa-brands fa-linkedin"></i>
+                    </a>
+
+                    <a href='#'>
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+
+                   
+
+                </div>
+             </div>
+          </div>
+    </div>
+  )
+}
