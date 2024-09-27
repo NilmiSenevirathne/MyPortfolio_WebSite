92f0d16f3056513e6bd587493f2df2307c1d07ac
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-09-28 00:33:36 +0530

    create About.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
+      <About/>
     </>
   );
 }
