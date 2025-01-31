24aecd02f725d7b5f4562b971fd085fda10cf868
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2025-01-31 10:35:05 +0530

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { ArrowRightCircle, Justify, JustifyLeft } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -55,8 +55,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Ama Senevirathne`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ " Full Stack Developer " ]'><span className="wrap">{text}</span></span></h1>
+                  <p >I am self-motivated, reliable, responsible, and hard-working person. I am a team worker and can adapt to all challenging situations. I have foundation in programming languages such as Laraval, PHP, React.js, Spring boot, java, C and mysql. I hope to become a professional in the field of information and Communication Systems with all the technica; and knowledge skills.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
